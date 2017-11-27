b10ebdd3e7aa7f8247445211c66781b2fa47c69e
JustRyanW <xryanjw@gmail.com>
2017-11-27 14:18:50 +0000

    UseSheild Script
    added methods, not working yet

--- Portfolio Game/Assets/Scripts/UseShield.cs
+++ Portfolio Game/Assets/Scripts/UseShield.cs
@@ -2,14 +2,12 @@
 using System.Collections;
 
 public class UseShield : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
 	
-	}
-	
-	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetButton("Fire1"))
+		{
+			
+		}
+
 	}
 }
