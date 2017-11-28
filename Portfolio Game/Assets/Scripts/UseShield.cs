fe1d0b9dab2bbc68e99b3864efe9a4f4b1c69fe2
Ryan <xryanjw@gmail.com>
2017-11-28 13:03:01 +0000

    Sheild Script
    sheild script now working

--- Portfolio Game/Assets/Scripts/UseShield.cs
+++ Portfolio Game/Assets/Scripts/UseShield.cs
@@ -2,12 +2,17 @@
 using System.Collections;
 
 public class UseShield : MonoBehaviour {
+
+    public GameObject Sheild;
 	
 	void Update () {
-		if (Input.GetButton("Fire1"))
+		if (Input.GetButton("Fire2"))
 		{
-			
-		}
+            Sheild.SetActive(true);
+        } else
+        {
+            Sheild.SetActive(false);
+        }
 
 	}
 }
