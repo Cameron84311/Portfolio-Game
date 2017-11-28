92a93d6bda213c543a12b2afafaee0ca2aa240e1
Ryan <xryanjw@gmail.com>
2017-11-28 13:24:17 +0000

    sheild changes

--- Portfolio Game/Assets/Scripts/UseShield.cs
+++ Portfolio Game/Assets/Scripts/UseShield.cs
@@ -1,20 +1,19 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class UseShield : MonoBehaviour {
 
-    public GameObject Sheild;
+    public GameObject sheild;
 	
 	void Update () {
 		if (Input.GetButton("Fire2"))
 		{
-            Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(Sheild.transform.position);
+            Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(sheild.transform.position);
             float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-            Sheild.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-            Sheild.SetActive(true);
+            sheild.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            sheild.SetActive(true);
         } else
         {
-            Sheild.SetActive(false);
+            sheild.SetActive(false);
         }
 
 	}
