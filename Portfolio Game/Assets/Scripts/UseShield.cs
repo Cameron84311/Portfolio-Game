83e7b9ee6554a0c57e23e2b1afd942fd0df56f23
Ryan <xryanjw@gmail.com>
2017-11-28 13:08:04 +0000

    sheild rotation optomization
    mereged sheild rotation script with sheild use script

--- Portfolio Game/Assets/Scripts/UseShield.cs
+++ Portfolio Game/Assets/Scripts/UseShield.cs
@@ -8,6 +8,9 @@ public class UseShield : MonoBehaviour {
 	void Update () {
 		if (Input.GetButton("Fire2"))
 		{
+            Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(Sheild.transform.position);
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            Sheild.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
             Sheild.SetActive(true);
         } else
         {
