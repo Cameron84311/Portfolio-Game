60b6964e551caf2c1725369e7d37a86753fac6ec
Ryan <xryanjw@gmail.com>
2017-11-29 16:31:11 +0000

    sprite updates

--- Portfolio Game/Assets/Scripts/PlayerMovement.cs
+++ Portfolio Game/Assets/Scripts/PlayerMovement.cs
@@ -2,13 +2,27 @@
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
+    private Rigidbody2D rb;
 
-	public float speed = 2;
+    public float speed = 2;
 	
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
 	void Update(){
 		Vector3 moveDir = Vector3.zero;
 		moveDir.x = Input.GetAxis("Horizontal");
 		moveDir.y = Input.GetAxis("Vertical");
 		transform.position += moveDir * speed * Time.deltaTime;
-	}
+        Debug.Log(rb.velocity.x);
+        // if (rb.velocity.x <= 0)
+        //{
+        //     spriteRenderer.flipX = true;
+        // } else
+        //{
+        //    spriteRenderer.flipX = false;
+        //}
+    }
 }
--- Portfolio Game/Assets/Scripts/UseShield.cs
+++ Portfolio Game/Assets/Scripts/UseShield.cs
@@ -5,29 +5,40 @@ public class UseShield : MonoBehaviour {
 	public GameObject sheild;
 	public float rotationSpeed = 5f;
 	public float joystickDeadzone = 0.2f;
-
-	float xAxis;
+    private SpriteRenderer spriteRenderer;
+    public Sprite sheildDown;
+    public Sprite sheildOut;
+    
+    float xAxis;
 	float yAxis;
-	
-	void Update () {
-		this.transform.position = sheild.transform.position;
-		if (Input.GetButton("Fire2"))
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update () {
+        
+        if (Input.GetButton("Fire2"))
 		{
-			Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(sheild.transform.position);
+            spriteRenderer.sprite = sheildOut;
+            Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(sheild.transform.position);
             float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
             sheild.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
             sheild.SetActive(true);
         } else
         {
+            spriteRenderer.sprite = sheildDown;
             sheild.SetActive(false);
         }
 		if (Mathf.Abs (Input.GetAxis ("Horizontal2")) >= joystickDeadzone || Mathf.Abs (Input.GetAxis ("Vertical2")) >= joystickDeadzone)
 		{
-			sheild.SetActive(true);
+            spriteRenderer.sprite = sheildOut;
+            sheild.SetActive(true);
 			xAxis = Input.GetAxis("Horizontal2");
 			yAxis = Input.GetAxis("Vertical2");
 			float joystickAngle = Mathf.Atan2(xAxis, yAxis) * Mathf.Rad2Deg;
-			sheild.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0,0,(-joystickAngle)+ 90), 1);
+			sheild.transform.rotation = Quaternion.Slerp(sheild.transform.rotation, Quaternion.Euler(0,0,(-joystickAngle)+ 90), 1);
 		}
-	}
+    }
 }
