eab3eab756737280de8fc12c05fa9111bdde74e3
Ryan <xryanjw@gmail.com>
2017-11-29 16:43:14 +0000

    player movement update

--- Portfolio Game/Assets/Scripts/PlayerMovement.cs
+++ Portfolio Game/Assets/Scripts/PlayerMovement.cs
@@ -2,8 +2,8 @@
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
-    private Rigidbody2D rb;
 
+    private Rigidbody2D rb;
     public float speed = 2;
 	
     void Start()
@@ -12,11 +12,14 @@ public class PlayerMovement : MonoBehaviour {
     }
 
 	void Update(){
-		Vector3 moveDir = Vector3.zero;
+        
+        Vector3 moveDir = Vector3.zero;
 		moveDir.x = Input.GetAxis("Horizontal");
 		moveDir.y = Input.GetAxis("Vertical");
-		transform.position += moveDir * speed * Time.deltaTime;
+        transform.position += moveDir * speed * Time.deltaTime;
+
         Debug.Log(rb.velocity.x);
+        
         // if (rb.velocity.x <= 0)
         //{
         //     spriteRenderer.flipX = true;
