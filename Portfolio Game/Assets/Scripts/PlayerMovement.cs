fcc0dc5c844f2ec8688dc0bf2f10e0f474df1e4a
Ryan <xryanjw@gmail.com>
2017-12-04 11:00:46 +0000

    animations

--- Portfolio Game/Assets/Scripts/PlayerMovement.cs
+++ Portfolio Game/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,13 @@ public class PlayerMovement : MonoBehaviour {
     private Rigidbody2D rb;
     public float speed = 2;
     private SpriteRenderer spriteRenderer;
+    public Sprite walk0;
+    public Sprite walk1;
+    public Sprite walk2;
+    public Sprite walk3;
+    public Sprite walk4;
+    public Sprite walk5;
+    public float animSpeed = 10f;
 
     void Start()
     {
@@ -13,7 +20,7 @@ public class PlayerMovement : MonoBehaviour {
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-	void Update(){
+    void Update(){
         
         Vector3 moveDir = Vector3.zero;
 		moveDir.x = Input.GetAxis("Horizontal");
@@ -27,5 +34,28 @@ public class PlayerMovement : MonoBehaviour {
         {
             spriteRenderer.flipX = false;
         }
+        if (Mathf.Abs(moveDir.x) > Mathf.Abs(moveDir.y))
+        {
+            speed = Mathf.Abs(moveDir.x);
+            StartCoroutine(waiter(speed));
+        }
+    }
+IEnumerator waiter(float speed)
+    {
+        while (true) {
+            spriteRenderer.sprite = walk0;
+            yield return new WaitForSeconds(animSpeed);
+            spriteRenderer.sprite = walk1;
+            yield return new WaitForSeconds(animSpeed);
+            spriteRenderer.sprite = walk2;
+            yield return new WaitForSeconds(animSpeed);
+            spriteRenderer.sprite = walk3;
+            yield return new WaitForSeconds(animSpeed);
+            spriteRenderer.sprite = walk4;
+            yield return new WaitForSeconds(animSpeed);
+            spriteRenderer.sprite = walk5;
+            yield return new WaitForSeconds(animSpeed);
+        }
     }
 }
+
--- Portfolio Game/Assets/Scripts/UseShield.cs
+++ Portfolio Game/Assets/Scripts/UseShield.cs
@@ -5,35 +5,35 @@ public class UseShield : MonoBehaviour {
 	public GameObject sheild;
 	public float rotationSpeed = 5f;
 	public float joystickDeadzone = 0.2f;
-    private SpriteRenderer spriteRenderer;
-    public Sprite sheildDown;
-    public Sprite sheildOut;
+    //private SpriteRenderer spriteRenderer;
+    //public Sprite sheildDown;
+    //public Sprite sheildOut;
     
     float xAxis;
 	float yAxis;
 
-    void Start()
-    {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-    }
+    //void Start()
+    //{
+        //spriteRenderer = GetComponent<SpriteRenderer>();
+    //}
 
     void Update () {
         
         if (Input.GetButton("Fire2"))
 		{
-            spriteRenderer.sprite = sheildOut;
+            //spriteRenderer.sprite = sheildOut;
             Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(sheild.transform.position);
             float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
             sheild.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
             sheild.SetActive(true);
         } else
         {
-            spriteRenderer.sprite = sheildDown;
+            //spriteRenderer.sprite = sheildDown;
             sheild.SetActive(false);
         }
 		if (Mathf.Abs (Input.GetAxis ("Horizontal2")) >= joystickDeadzone || Mathf.Abs (Input.GetAxis ("Vertical2")) >= joystickDeadzone)
 		{
-            spriteRenderer.sprite = sheildOut;
+            //spriteRenderer.sprite = sheildOut;
             sheild.SetActive(true);
 			xAxis = Input.GetAxis("Horizontal2");
 			yAxis = Input.GetAxis("Vertical2");
