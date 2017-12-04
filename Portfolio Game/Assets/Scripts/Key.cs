42d158d9328468bc0448a8431e65466a21aebc3d
RobertTemple <roberttemple2001@gmail.com>
2017-12-04 14:00:50 +0000

    key

---
+++ Portfolio Game/Assets/Scripts/Key.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Key : MonoBehaviour {
+
+	//Makes the dooe a variable
+	public GameObject door;
+
+	void OnCollisionEnter () {
+		//When the key is touched the door will be destoyed
+		Destroy(door);
+		//Will also destroy the key
+		Destroy(gameObject);
+	}
+}
