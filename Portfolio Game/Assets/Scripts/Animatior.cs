412830b86a4b9a1a44e930ca1380f92dfc362cd2
Ryan <xryanjw@gmail.com>
2017-12-05 11:30:40 +0000

    animator script

---
+++ Portfolio Game/Assets/Scripts/Animatior.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animatior : MonoBehaviour {
+
+    private SpriteRenderer spriteRenderer;
+    private PlayerMovement movement;
+    public Sprite idle;
+    public Sprite walk0;
+    public Sprite walk1;
+    public Sprite walk2;
+    public Sprite walk3;
+    public Sprite walk4;
+    public Sprite walk5;
+    public float animSpeed = 0.2f;
+    public float moveX;
+    public float moveY;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        movement = GetComponent<PlayerMovement>();
+        StartCoroutine(waiter());
+    }
+
+	void Update () {
+        if (movement.moveX < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (movement.moveX > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        if (moveX == 0)
+        {
+            spriteRenderer.sprite = idle;
+        }
+        moveX = Mathf.Abs(movement.moveX);
+        moveY = Mathf.Abs(movement.moveY);
+	}
+    
+    IEnumerator waiter()
+    {
+        while (true)
+        {
+            if (moveX > moveY)
+            {
+                spriteRenderer.sprite = walk0;
+            }
+            yield return new WaitForSeconds(animSpeed);
+            if (moveX > moveY)
+            {
+                spriteRenderer.sprite = walk1;
+            }
+            yield return new WaitForSeconds(animSpeed);
+            if (moveX > moveY)
+            {
+                spriteRenderer.sprite = walk2;
+            }
+            yield return new WaitForSeconds(animSpeed);
+            if (moveX > moveY)
+            {
+                spriteRenderer.sprite = walk3;
+            }
+            yield return new WaitForSeconds(animSpeed);
+            if (moveX > moveY)
+            {
+                spriteRenderer.sprite = walk4;
+            }
+            yield return new WaitForSeconds(animSpeed);
+            if (moveX > moveY)
+            {
+                spriteRenderer.sprite = walk5;
+            }
+            yield return new WaitForSeconds(animSpeed);
+        }
+    }
+}
--- Portfolio Game/Assets/Scripts/PlayerMovement.cs
+++ Portfolio Game/Assets/Scripts/PlayerMovement.cs
@@ -4,24 +4,8 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 
     public float speed = 2;
-    private SpriteRenderer spriteRenderer;
-	public Sprite idle;
-	public Sprite walk0;
-    public Sprite walk1;
-    public Sprite walk2;
-    public Sprite walk3;
-    public Sprite walk4;
-    public Sprite walk5;
-    public float animSpeed = 10f;
 	public float moveX;
 	public float moveY;
-	public float waitTime;
-
-    void Start()
-    {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-		StartCoroutine (waiter ());
-    }
 
     void Update(){
         
@@ -29,52 +13,8 @@ public class PlayerMovement : MonoBehaviour {
 		moveDir.x = Input.GetAxis("Horizontal");
 		moveDir.y = Input.GetAxis("Vertical");
         transform.position += moveDir * speed * Time.deltaTime;
-        
-        if (moveDir.x < 0)
-        {
-            spriteRenderer.flipX = true;
-		} else if (moveDir.x > 0)
-        {
-            spriteRenderer.flipX = false;
-        }
-		if (moveX == 0) {
-			spriteRenderer.sprite = idle;
-		}
-		moveX = Mathf.Abs (moveDir.x);
-		moveY = Mathf.Abs (moveDir.y);
+        moveX = moveDir.x;
+        moveY = moveDir.y;
 	}
-
-
-
-	IEnumerator waiter()
-    {
-		while (true){
-			
-			if (moveX > moveY) {
-				spriteRenderer.sprite = walk0;
-			}
-			yield return new WaitForSeconds (animSpeed);
-			if (moveX > moveY) {
-				spriteRenderer.sprite = walk1;
-			}
-			yield return new WaitForSeconds (animSpeed);
-			if (moveX > moveY) {
-				spriteRenderer.sprite = walk2;
-			}
-			yield return new WaitForSeconds (animSpeed);
-			if (moveX > moveY) {
-				spriteRenderer.sprite = walk3;
-			}
-			yield return new WaitForSeconds (animSpeed);
-			if (moveX > moveY) {
-				spriteRenderer.sprite = walk4;
-			}
-			yield return new WaitForSeconds (animSpeed);
-			if (moveX > moveY) {
-				spriteRenderer.sprite = walk5;
-			}
-			yield return new WaitForSeconds (animSpeed);
-        }
-    }
 }
 
