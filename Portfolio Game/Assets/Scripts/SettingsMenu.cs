dee9a5564572c5ad694997d831d2ef634a011893
RobertTemple <roberttemple2001@gmail.com>
2017-12-06 22:55:57 +0000

    Options 2.0

--- Portfolio Game/Assets/Scripts/SettingsMenu.cs
+++ Portfolio Game/Assets/Scripts/SettingsMenu.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 
@@ -18,16 +19,28 @@ public class SettingsMenu : MonoBehaviour {
         resolutionDropdown.ClearOptions();
 
         List<string> options = new List<string>();
-        
-        for (int i = 0; < resolutions.Length; i++)
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + " x " + resolutions[i].height;
             options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
         }
 
-
-
         resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
     public void SetVolume(float volume)
