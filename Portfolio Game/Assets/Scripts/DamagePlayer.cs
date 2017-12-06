c214d399178b4d7d9fe95f3a47c2fc552bbb5424
Ryan <xryanjw@gmail.com>
2017-12-06 11:26:01 +0000

    Shield blocks damage

--- Portfolio Game/Assets/Scripts/DamagePlayer.cs
+++ Portfolio Game/Assets/Scripts/DamagePlayer.cs
@@ -2,6 +2,7 @@
 
 public class DamagePlayer : MonoBehaviour {
 
+    public GameObject Shield;
     public GameObject healthGUI;
     private PlayerHealth playerHealth;
 
@@ -13,7 +14,7 @@ public class DamagePlayer : MonoBehaviour {
     // If the gameObject collides with the player then do dammage
     void OnCollisionEnter2D(Collision2D coll)
     {
-        if (coll.gameObject.tag == "Player")
+        if (coll.gameObject.tag == "Player" &! Shield.activeSelf)
         {
             playerHealth.DamagePlayer();
         }
