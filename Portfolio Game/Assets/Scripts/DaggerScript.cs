44dc06c85754aefbdf9688ac28a374901c1ab165
Ryan <xryanjw@gmail.com>
2017-12-12 14:54:33 +0000

    fixed dagger

--- Portfolio Game/Assets/Scripts/DaggerScript.cs
+++ Portfolio Game/Assets/Scripts/DaggerScript.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class DaggerScript : MonoBehaviour {
 
     public float rotationSpeed = 10f;
+    public float damage = 1f;
 
 	void Start () {
         Destroy(gameObject, 5);
@@ -12,4 +13,16 @@ public class DaggerScript : MonoBehaviour {
 	void Update () {
         gameObject.GetComponent<Rigidbody2D>().rotation = -rotationSpeed * Time.frameCount;
 	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Enemy")
+        {
+            coll.gameObject.GetComponent<SpaceMineController>();
+
+        } else
+        {
+            Destroy(gameObject);
+        }
+    }
 }
