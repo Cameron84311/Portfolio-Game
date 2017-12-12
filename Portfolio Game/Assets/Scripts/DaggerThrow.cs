2a0d4ce3672bdbf21415590d9a315c6fdbe23771
Ryan <xryanjw@gmail.com>
2017-12-12 13:17:53 +0000

    Dagger Thow
    + Dagger its now thrown towards the mouse.

--- Portfolio Game/Assets/Scripts/DaggerThrow.cs
+++ Portfolio Game/Assets/Scripts/DaggerThrow.cs
@@ -1,22 +1,26 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
 
 public class DaggerThrow : MonoBehaviour {
 
+    private Vector3 mousePos;
+    private Vector2 mouseDir;
     public GameObject daggerPrefab;
     public GameObject delaySlider;
-    public float  reloadTime = 200f;
+    public float reloadTime = 200f;
+    public float throwPower = 10f;
     private float delay;
-    public float horizontalVelocity = 5f;
-    public float verticalVelocity = 5f;
 
 	void Update () {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        mouseDir = mousePos - gameObject.transform.position;
+        mouseDir = mouseDir.normalized;
 
         if (Input.GetButton("Fire1") && delay <= 0)
         {           
             GameObject dagger = (GameObject)Instantiate(daggerPrefab, gameObject.transform.position, gameObject.transform.rotation);
-            dagger.GetComponent<Rigidbody2D>().velocity = new Vector2(horizontalVelocity, verticalVelocity);
+            dagger.GetComponent<Rigidbody2D>().velocity = mouseDir * throwPower;
             delay = reloadTime;
         }
         delaySlider.GetComponent<Image>().fillAmount = delay/reloadTime;
