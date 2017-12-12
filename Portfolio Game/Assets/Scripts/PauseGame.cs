c56c8cb982d8888bb80e690040ebc8f92cd4fb39
RobertTemple <roberttemple2001@gmail.com>
2017-12-12 14:23:41 +0000

    Pause Menu

---
+++ Portfolio Game/Assets/Scripts/PauseGame.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseGame : MonoBehaviour {
+    public Transform canvas;
+
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if ( canvas.gameObject.activeInHierarchy == false)
+            {
+                canvas.gameObject.SetActive(true);
+            } else
+            {
+                canvas.gameObject.SetActive(false);
+            }
+        }
+	}
+}
