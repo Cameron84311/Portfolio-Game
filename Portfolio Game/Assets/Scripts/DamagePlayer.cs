aa6f1b921206ea33849458aaea36dc0ab421d831
Ryan <xryanjw@gmail.com>
2017-12-12 15:20:21 +0000

    DamagePlayer script
    + Added notations

--- Portfolio Game/Assets/Scripts/DamagePlayer.cs
+++ Portfolio Game/Assets/Scripts/DamagePlayer.cs
@@ -2,13 +2,16 @@
 
 public class DamagePlayer : MonoBehaviour {
 
-    // If the gameObject collides with the player then do dammage
+    // Checks for a collision and applies the collided gameObject to coll.
     void OnCollisionEnter2D(Collision2D coll)
     {
+        // Checks if coll has the "Player" tag.
         if (coll.gameObject.tag == "Player")
         {
+            // Checks if coll has a child calld "Shield" and if it is not active.
             if (!coll.gameObject.transform.Find("Shield").gameObject.activeSelf)
             {
+                // Calls the DamagePlayer()  method on the collided player.
                 coll.gameObject.GetComponent<PlayerHealth>().DamagePlayer();
             }
         }
