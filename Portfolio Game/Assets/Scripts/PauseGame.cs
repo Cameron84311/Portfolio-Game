0f9497bc5c82efab14a6ed726adb18b4c2e874da
RobertTemple <roberttemple2001@gmail.com>
2017-12-12 14:30:49 +0000

    Pause

--- Portfolio Game/Assets/Scripts/PauseGame.cs
+++ Portfolio Game/Assets/Scripts/PauseGame.cs
@@ -12,9 +12,11 @@ public class PauseGame : MonoBehaviour {
             if ( canvas.gameObject.activeInHierarchy == false)
             {
                 canvas.gameObject.SetActive(true);
+                Time.timeScale = 0;
             } else
             {
                 canvas.gameObject.SetActive(false);
+                Time.timeScale = 1;
             }
         }
 	}
