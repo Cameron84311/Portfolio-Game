9e54b8412ac6c001512e0f7f7a3ac91feb8deaa2
RobertTemple <roberttemple2001@gmail.com>
2017-12-13 13:47:31 +0000

    delete level 1

---
+++ Portfolio Game/Assets/Scripts/GoalController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GoalController : MonoBehaviour
+{
+    void OnCollisionEnter2D()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
